094ed40b2dc43f3e3a0b9bad0544bd789bc02807
Tyler Grant <tgrant5@sorenson.com>
2024-05-19 11:16:07 -0600

    testing action

--- adventure-granted/app/page.js
+++ adventure-granted/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div>WELCOME TO ADVENTURE GRANTED</div>
+      <div>WELCOME TO ADVENTURE GRANTED TEST</div>
     </main>
   );
 }
